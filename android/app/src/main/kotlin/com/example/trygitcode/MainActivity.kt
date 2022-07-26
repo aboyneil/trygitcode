9d77186d776b98f11e58f00b0be4bf8aab8f0245
aboyneil <neiljoshuamatias03@gmail.com>
2022-07-26 10:29:14 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/trygitcode/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trygitcode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Aboy Try Application",
+      home: Home(),
+    ),
+  );
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
